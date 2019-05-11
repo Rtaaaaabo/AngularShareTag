2db597b56523b6dde5d40b772084a3ab81d897a7
Taku Nakagawa <taku_nakagawa@change-jp.com>
2019-05-11 21:28:57 +0900

    Modileの追加

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    // CommonModule
+    BrowserAnimationsModule
+  ],
+  exports: [
+    BrowserAnimationsModule
+  ]
+})
+export class MaterialModule { }
